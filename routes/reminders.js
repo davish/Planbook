e0a2703c6b3a49cb5653725a51e99f3a88e8cf80
Davis Haupt <dbh937@gmail.com>
2014-11-02 20:53:14 -0500

    closed #11 reminders implemented on backend

--- routes/reminders.js
+++ routes/reminders.js
@@ -3,44 +3,82 @@ module.exports = {
   'new Date().toISOString().slice(0,10)' and return it in JSON.*/
   get: function(req, res) {
     res.type('text/json');
-    /*db.collection("reminders").find({
+    db.collection("reminders").find({
       'name': req.session.username, 
       'reminderDate': new Date(req.param('today')).toISOString().slice(0,10)
-    }).toArray(function(err, docs) {
-      if (!err)
-        res.send(docs);
+    }).toArray(function(err, reminders) {
+      if (!err) { // gotta get the contents of the reminders, since they're not static.
+        var weeks = {}; // construct object with the weeks as the key and the reminders in an array as the value
+        for (var i = 0; i < reminders.length; i++) {
+          // dont' need these two fields
+          delete reminders[i]._id;
+          delete reminders[i].name;
+          // add it to the object
+          if (weeks[reminders[i].monday])
+            weeks[reminders[i].monday].push(reminders[i]);
+          else
+            weeks[reminders[i].monday] = [reminders[i]];
+        }
+        for (var key in weeks) { // loop through, add the contents to the reminder objects.
+          if (weeks.hasOwnProperty(key)) {
+            db.collection("assignments").find({
+              name: req.session.username, monday: key
+            }).toArray(function(err, docs) {
+              if (docs) { // if the week exists
+                var data = JSON.parse(docs[0].data);
+                for (var i = 0; i < weeks[key].length; i++)
+                  weeks[key][i]['description'] = data[weeks[key][i].box][0];
+                res.send(reminders); // taking advantage of the mutability of objects in JS
+              }
+            });
+          }
+        }
+      }
       else
         res.send(500, err);
-    });*/
-    res.send(404, {'message': 'Coming Soon'});
+    });
   },
   /* add 1 or a batch or reminders in the valid JSON format to the db with req.session.username and req.body.reminders.
      if action=add, then add them. if action=remove, then remove them.*/
   post: function(req, res) {
     res.type('text/json');
-    /*// Count backwards from the duedate, subtracting the number of days in the interval, so you get a date 
-    var dueDate = req.body.dueDate;
-    var startReminders = new Date(req.body.dueDate);
-    startReminders.setDate(startReminders.getDate()-req.body.options.startReminding);
+    
+    // calculate day to start reminding you
+    var m = new Date(req.body.monday);
+    var startReminders = new Date(m.getFullYear(), 
+      m.getMonth(), 
+      m.getDate() + ((req.body.box.split('').reverse()[0]-1) - req.body.options.startReminding));
+
+    // remove existing reminders for this box so that there's no dublicates.
+    db.collection("reminders").removeMany({
+      $and: [{name: req.session.username}, {box: req.body.box}, {monday: req.body.monday}]
+    }, function(err, result) {
+      if (!err) {
+        var reminders = [];
+
+        var dueDate = new Date(m.getFullYear(), 
+          m.getMonth(), 
+          m.getDate() + parseInt(req.body.box.split('').reverse()[0])-1);
 
 
-    var reminders = [];
-
-    for (var d = new Date(dueDate); d.getTime() > startReminders.getTime(); d.setDate(d.getDate() - req.body.options.interval)) {
-      reminders.push({name: req.session.username, 
-        subject: req.body.subject, 
-        dueDate: dueDate, 
-        details: req.body.details, 
-        reminderDate: d.toISOString().slice(0,10)
-      });
-    }
-
-    db.collection("reminders").insert(reminders, function(err, result) {
-      if (!err)
-        res.end(200);
-      else
+        for (var d = dueDate; d.getTime() > startReminders.getTime(); d.setDate(d.getDate() - req.body.options.interval)) {
+          reminders.push({
+            name: req.session.username, 
+            box: req.body.box, 
+            monday: req.body.monday,
+            reminderDate: d.toISOString().slice(0,10)
+          });
+        }
+        var r = db.collection("reminders")
+        r.insert(reminders, function(err, result) {
+          if (!err)
+            res.send(200);
+          else
+            res.send(500, err);
+        });
+      } else {
         res.end(500, err);
-    });*/
-    res.send(404, {'message': 'Coming Soon'});
+      }
+    });
   }
 };
