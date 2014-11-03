a4484e132a76d109090f03665c5305919228455a
Davis Haupt <dbh937@gmail.com>
2014-11-02 23:25:02 -0500

    finished

--- maintenance.js
+++ maintenance.js
@@ -4,3 +4,7 @@ var express = require("express"),
 app.get('/', function(req, res) {
   res.send("<h1>The Planbook is undergoing maintenance.</h1><h3>We thank you for your patience and hope to resume service shortly.</h3>");
 });
+
+var server = app.listen(app.get('port'), function() {
+  console.info('Listening on port %d', server.address().port);
+});
