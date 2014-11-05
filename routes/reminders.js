5cf0c216f1d127af18a941893a715be901caa8ea
Davis Haupt <dbh937@gmail.com>
2014-11-04 19:41:43 -0500

    updated getting reminders to not be so complicated

--- routes/reminders.js
+++ routes/reminders.js
@@ -7,33 +7,8 @@ module.exports = {
       'name': req.session.username, 
       'reminderDate': new Date(req.param('today')).toISOString().slice(0,10)
     }).toArray(function(err, reminders) {
-      if (!err) { // gotta get the contents of the reminders, since they're not static.
-        var weeks = {}; // construct object with the weeks as the key and the reminders in an array as the value
-        for (var i = 0; i < reminders.length; i++) {
-          // dont' need these two fields
-          delete reminders[i]._id;
-          delete reminders[i].name;
-          // add it to the object
-          if (weeks[reminders[i].monday])
-            weeks[reminders[i].monday].push(reminders[i]);
-          else
-            weeks[reminders[i].monday] = [reminders[i]];
-        }
-        for (var key in weeks) { // loop through, add the contents to the reminder objects.
-          if (weeks.hasOwnProperty(key)) {
-            db.collection("assignments").find({
-              name: req.session.username, monday: key
-            }).toArray(function(err, docs) {
-              if (docs) { // if the week exists
-                var data = JSON.parse(docs[0].data);
-                for (var i = 0; i < weeks[key].length; i++)
-                  weeks[key][i]['description'] = data[weeks[key][i].box][0];
-                res.send(reminders); // taking advantage of the mutability of objects in JS
-              }
-            });
-          }
-        }
-      }
+      if (!err) // gotta get the contents of the reminders, since they're not static.
+        res.send(reminders);
       else
         res.send(500, err);
     });
@@ -44,37 +19,44 @@ module.exports = {
     res.type('text/json');
     
     // calculate day to start reminding you
-    var m = new Date(req.body.monday);
-    var startReminders = new Date(m.getFullYear(), 
-      m.getMonth(), 
-      m.getDate() + ((req.body.box.split('').reverse()[0]-1) - req.body.options.startReminding));
+    
 
     // remove existing reminders for this box so that there's no dublicates.
     db.collection("reminders").removeMany({
       $and: [{name: req.session.username}, {box: req.body.box}, {monday: req.body.monday}]
     }, function(err, result) {
       if (!err) {
-        var reminders = [];
+        if (req.body.options) { // if there aren't options the client just wants to delete reminders
+          var m = new Date(req.body.monday);
+          var startReminders = new Date(m.getFullYear(), 
+            m.getMonth(), 
+            m.getDate() + ((req.body.box.split('').reverse()[0]-1) - req.body.options.startReminding));
 
-        var dueDate = new Date(m.getFullYear(), 
-          m.getMonth(), 
-          m.getDate() + parseInt(req.body.box.split('').reverse()[0])-1);
-        var dd = dueDate.toISOString().slice(0,10);
-        for (var d = dueDate; d.getTime() > startReminders.getTime(); d.setDate(d.getDate() - req.body.options.interval)) {
-          reminders.push({
-            name: req.session.username, 
-            box: req.body.box, 
-            monday: req.body.monday,
-            dueDate: dd,
-            reminderDate: d.toISOString().slice(0,10)
+          var dueDate = new Date(m.getFullYear(), 
+            m.getMonth(), 
+            m.getDate() + parseInt(req.body.box.split('').reverse()[0]));
+          var dd = dueDate.toISOString().slice(0,10);
+          
+          var reminders = [];
+          for (var d = dueDate; d.getTime() > startReminders.getTime(); d.setDate(d.getDate() - req.body.options.interval)) {
+            reminders.push({
+              name: req.session.username, 
+              box: req.body.box, 
+              monday: req.body.monday,
+              dueDate: dd,
+              reminderDate: d.toISOString().slice(0,10),
+              description: req.body.description
+            });
+          }
+          db.collection("reminders").insert(reminders, function(err, result) {
+            if (!err)
+              res.send(200);
+            else
+              res.send(500, err);
           });
+        } else {
+          res.send(200);
         }
-        db.collection("reminders").insert(reminders, function(err, result) {
-          if (!err)
-            res.send(200);
-          else
-            res.send(500, err);
-        });
       } else {
         res.end(500, err);
       }
