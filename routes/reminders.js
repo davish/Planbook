ce32c105fbe6ecb5a8aad2e70685e6532832a4ee
Davis Haupt <dbh937@gmail.com>
2014-11-15 21:29:19 -0500

    simplified reminders, now only one entry per reminder in the DB

--- routes/reminders.js
+++ routes/reminders.js
@@ -4,8 +4,9 @@ module.exports = {
   get: function(req, res) {
     res.type('text/json');
     db.collection("reminders").find({
-      'name': req.session.username, 
-      'reminderDate': new Date(req.param('today')).toISOString().slice(0,10)
+      name: req.session.username,
+      dueDate: {$gte: new Date(req.param('today'))},
+      startReminding: {$lt: new Date(req.param('today'))}
     }).toArray(function(err, reminders) {
       if (!err) // gotta get the contents of the reminders, since they're not static.
         res.send(reminders);
@@ -17,10 +18,6 @@ module.exports = {
      if action=add, then add them. if action=remove, then remove them.*/
   post: function(req, res) {
     res.type('text/json');
-    
-    // calculate day to start reminding you
-    
-
     // remove existing reminders for this box so that there's no dublicates.
     db.collection("reminders").removeMany({
       $and: [{name: req.session.username}, {box: req.body.box}, {monday: req.body.monday}]
@@ -39,20 +36,17 @@ module.exports = {
             dueDate.getMonth(), 
             dueDate.getDate() - (parseInt(req.body.options.startReminding)+1));
 
-          var dd = dueDate.toISOString().slice(0,10);
+          var reminders = {
+            startReminding: startReminders,
+            dueDate: dueDate,
 
-          var reminders = [];
-          for (var d = dueDate; d.getTime() > startReminders.getTime(); d.setDate(d.getDate() - req.body.options.interval)) {
-            reminders.push({
-              name: req.session.username, 
-              box: req.body.box, 
-              monday: req.body.monday,
-              colorCode: req.body.colorCode,
-              dueDate: dd,
-              reminderDate: d.toISOString().slice(0,10),
-              description: req.body.description
-            });
-          }
+            monday: req.body.monday,
+            box: req.body.box,
+            description: req.body.description,
+            colorCode: req.body.colorCode,
+
+            name: req.session.username
+          };
           db.collection("reminders").insert(reminders, function(err, result) {
             if (!err)
               res.send(200);
