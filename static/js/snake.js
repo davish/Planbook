abea4931f715a06b9da6d4da0a60416cf669132f
davis <dbh937@gmail.com>
2015-05-22 13:16:31 -0400

    snake.js is now a snake API

--- static/js/snake.js
+++ static/js/snake.js
@@ -1,198 +1,176 @@
-window.SQUARE_SIZE = 10;
-var SPEED = 75; // milliseconds delay between steps
+function Snake(id, size, speed, callbacks) {
+  this.callbacks = callbacks;
+  this.SQUARE_SIZE = size;
+  this.SPEED = speed; // milliseconds delay between steps 
+  this.boundaries = [];
 
-var add_x = 0, add_y = 0;
-var head = {x: 5, y: 5};
-var boundaries = [];
-var history = []; // places the snake's head has been
-var snakeLength = 0;
-var forbidden = [];
-var fruit = {};
- 
-
-function reset() {
-  // reset the board and snake variables
-  add_x = 0;
-  add_y = 0;
-  head = {x: 5, y: 5};
-  history = [];
-  snakeLength = 0;
-  fruit = {};
-
-  clearField();
-  makeFruit();
-  window.loop = setInterval(step, SPEED);
-
-}
-
-function pause() {
-  if (window.loop)
-    clearInterval(window.loop)
-  ctx.fillText("Press 'p' to unpause", 20, 20);
-  window.loop = undefined;
-}
-
-function resume() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  makeBorder();
-  if (!window.loop)
-    window.loop=setInterval(step, SPEED);
-}
-
-function initializeSnake(id){
-  // initialize the canvas and grid, with the width and height as parameters
-  window.canvas = document.getElementById(id);
-
-  window.grid_width = Math.floor(canvas.width / SQUARE_SIZE); 
-  window.grid_height = Math.floor(canvas.height / SQUARE_SIZE);
+  var canvas = document.getElementById(id);
+  this.grid_width = Math.floor(canvas.width / this.SQUARE_SIZE);
+  this.grid_height = Math.floor(canvas.height / this.SQUARE_SIZE);
+  this.ctx = null;
+  this.vals = {};
 
   if (canvas.getContext){
-    window.ctx = canvas.getContext('2d');
-    // drawGrid(SQUARE_SIZE); // For dev purposes
-    makeBorder();
-    makeFruit();
-    pause();
+    this.ctx = canvas.getContext('2d');
+    this.makeBorder();
+    this.reset();
   }
+  canvas.tabIndex = 1000;
+  canvas.addEventListener("keydown", this.onkeydown(this));
+  canvas.style.outline = "none";
 }
 
-function step() {
-  // function called by setInterval() to move one step forward in game
-  head.x += add_x;
-  head.y += add_y;
-
-  var body = snakeLength > 0 ? history.slice(-snakeLength) : [];
-  window.forbidden = boundaries.concat(body);
-
-  for (var i = 0; i < forbidden.length; i++) {
-    if (forbidden[i].x == head.x && forbidden[i].y == head.y) {
-      pause()
-
-    }
-    if (fruit.x == head.x && fruit.y == head.y) {
-      console.log(++snakeLength);
-      makeFruit();
-    }
-  }
-  updateSnake(body);
-
-}
-
-function makeFruit() {
-  // generate random coordinates and put a fruit on the grid
-  // grid_height grid_width
-  var rand_x = Math.round(Math.random() * grid_width - 1);
-  var rand_y = Math.round(Math.random() * grid_height - 1);
-
-  while (rand_x <= 0 || rand_x >= grid_width - 1) {
-    rand_x = Math.round(Math.random() * grid_width - 1);
-  }
-  while (rand_y <= 0 || rand_y >= grid_height - 1) {
-    rand_y = Math.round(Math.random() * grid_height - 1);
-  }
-
-  fruit.x = rand_x;
-  fruit.y = rand_y;
-
-  console.log(rand_x + "," + rand_y);
-
-}
-function gameOver() {
-  clearInterval(loop);
-
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // ctx.fillText("Press 'p' to unpause", 20, 20);
-
-  // ctx.fillText("Nice Try! you got " + snakeLength + " fruits!\n\nPress space to reset the game.", 40, 40)
-}
-
-function updateSnake(body) {
-  clearField();
+Snake.prototype.updateSnake = function(body) {
+  this.clearField();
   for (var i = 0; i < body.length; i++) {
-    fillSquare(body[i].x, body[i].y, true, "rgb(0, 100, 500)");
+    this.fillSquare(body[i].x, body[i].y, true, "rgb(0, 100, 500)");
   };
-  fillSquare(head.x, head.y, true, "rgb(0, 100, 500)");
-  history.push({x: head.x, y: head.y});
+  this.fillSquare(this.vals.head.x, this.vals.head.y, true, "rgb(0, 100, 500)");
+  this.vals.snakeHistory.push({x: this.vals.head.x, y: this.vals.head.y});
 
 }
 
-function clearField() {
-  // reset all blocks to white except for the boundaries
-  for (var x = 1; x < grid_width-1; x++) {
-    for(var y = 1; y < grid_height-1; y++) {
-      fillSquare(x,y,false);
-    }
-  }
-  fillSquare(fruit.x, fruit.y, true, "rgb(1000, 0, 0)");
-}
-
-function fillSquare(x, y, fill, color) {
+Snake.prototype.fillSquare = function(x, y, fill, color) {
   // fill in a square on the grid based on its x and y coordinates
   if (fill) {
-    ctx.fillStyle = color || "rgb(0,0,0)"
+    this.ctx.fillStyle = color || "rgb(0,0,0)"
   } else {
-    ctx.fillStyle = "rgb(1000, 1000, 1000)"
+    this.ctx.fillStyle = "rgb(1000, 1000, 1000)"
   }
-  ctx.fillRect(x * SQUARE_SIZE + 1, y * SQUARE_SIZE + 1, SQUARE_SIZE - 2, SQUARE_SIZE - 2);
-
+  this.ctx.fillRect(x * this.SQUARE_SIZE + 1, y * this.SQUARE_SIZE + 1, this.SQUARE_SIZE - 2, this.SQUARE_SIZE - 2);
 }
 
-window.onkeydown = function(event) {
+Snake.prototype.makeBorder = function() {
+  // fill in the border blocks as black
+  boundaries = []; // without this boundaries can get infinitely long, slowing down the game each time you reset.
+  for (var i = 0; i < this.grid_width; i++) {
+    this.fillSquare(i, 0, true);
+    this.boundaries.push({x: i, y: 0});
+    this.fillSquare(i, this.grid_height - 1, true);
+    this.boundaries.push({x: i, y: this.grid_height - 1});
+  }
+  for (var i = 0; i < this.grid_height; i++) {
+    this.fillSquare(0, i, true);
+    this.boundaries.push({x: 0, y: i});
+    this.fillSquare(this.grid_width - 1, i, true);
+    this.boundaries.push({x: this.grid_width - 1, y: i});
+  }
+}
+
+Snake.prototype.clearField = function() {
+  // reset all blocks to white except for the boundaries
+  for (var x = 1; x < this.grid_width-1; x++) {
+    for(var y = 1; y < this.grid_height-1; y++) {
+      this.fillSquare(x,y,false);
+    }
+  }
+  this.fillSquare(this.vals.fruit.x, this.vals.fruit.y, true, "rgb(1000, 0, 0)");
+}
+
+Snake.prototype.reset = function() {
+  // reset the board and snake variables
+  console.log(this.vals);
+  this.vals = {
+    add_x: 0,
+    add_y: 0,
+    head: {x: 5, y: 5},
+    snakeHistory: [],
+    snakeLength: 0,
+    fruit: {},
+    pressed: false
+  }
+  console.log(this.vals);
+
+  document.getElementById("score").innerHTML = this.vals.snakeLength;
+
+  this.clearField();
+  this.makeFruit();
+  this.makeBorder();
+  clearInterval(this.loop);
+  var l = this.step;
+  var t = this;
+  this.loop = setInterval(function() {
+    l(t);
+  }, this.SPEED);
+}
+
+Snake.prototype.step = function(t) {
+  // function called by setInterval() to move one step forward in game
+  t.vals.pressed = false;
+  t.vals.head.x += t.vals.add_x;
+  t.vals.head.y += t.vals.add_y;
+
+  var body = t.vals.snakeLength > 0 ? t.vals.snakeHistory.slice(-t.vals.snakeLength) : [];
+  var forbidden = t.boundaries.concat(body);
+
+  if (t.vals.fruit.x == t.vals.head.x && t.vals.fruit.y == t.vals.head.y) { // if you ate the fruit
+    document.getElementById("score").innerHTML = ++t.vals.snakeLength;
+    t.makeFruit(); // make a new one
+  }
+
+  for (var i = 0; i < forbidden.length; i++) { // check the head position against the array of stuff you can't hit in to
+    if (forbidden[i].x == t.vals.head.x && forbidden[i].y == t.vals.head.y) { // if you ran into a wall or yourself
+      console.log("dead");
+      t.gameOver();
+      return;
+    }
+  }
+  t.updateSnake(body);
+}
+
+Snake.prototype.makeFruit = function() {
+  var rand_x = Math.floor(Math.random() * (this.grid_width - 3)+1); // 50-3=47; 0 to 47+1 is 1 to 48
+  var rand_y = Math.floor(Math.random() * (this.grid_height - 3)+1);
+
+  while ((rand_x <= 0 || rand_x >= this.grid_width - 1)) {
+    rand_x = Math.round(Math.random() * this.grid_width - 1);
+  }
+  while (rand_y <= 0 || rand_y >= this.grid_height - 1) {
+    rand_y = Math.round(Math.random() * this.grid_height - 1);
+  }
+
+  this.vals.fruit.x = rand_x;
+  this.vals.fruit.y = rand_y;
+}
+
+Snake.prototype.gameOver = function() {
+  clearInterval(this.loop);
+  this.callbacks.gameover(this.vals.snakeLength);
+  this.makeBorder();
+}
+
+Snake.prototype.onkeydown = function(t) {
   // clockwise starting from left arrow 37-40
   // event handler
-  if (event.which == 37 && add_x != 1) {
-    // left
-    add_x = -1;
-    add_y = 0;
-    event.preventDefault();
-    event.returnValue = false;  
-  } else if (event.which == 38 && add_y != 1) {
-    // up
-    add_x = 0;
-    add_y = -1;
-    event.preventDefault();
-    event.returnValue = false;  
-  } else if (event.which == 39 && add_x != -1) {
-    // right
-    add_x = 1;
-    add_y = 0;
-    event.preventDefault();
-    event.returnValue = false;  
-  } else if (event.which == 40 && add_y != -1) {
-    // down
-    add_x = 0;
-    add_y = 1;
-    event.preventDefault();
-    event.returnValue = false;  
+  return function(event) {
+    if (event.which == 37 && (t.vals.snakeLength == 0|| t.vals.add_x != 1) && !t.vals.pressed) {
+      // left
+      t.vals.add_x = -1;
+      t.vals.add_y = 0;
+      event.preventDefault();
+      event.returnValue = false;
+      t.vals.pressed = true;
+    } else if (event.which == 38 && (t.vals.snakeLength == 0|| t.vals.add_y != 1) && !t.vals.pressed) {
+      // up
+      t.vals.add_x = 0;
+      t.vals.add_y = -1;
+      event.preventDefault();
+      event.returnValue = false;
+      t.vals.pressed = true;
+    } else if (event.which == 39 && (t.vals.snakeLength == 0|| t.vals.add_x != -1) && !t.vals.pressed) {
+      // right
+      t.vals.add_x = 1;
+      t.vals.add_y = 0;
+      event.preventDefault();
+      event.returnValue = false;
+      t.vals.pressed = true;
+    } else if (event.which == 40 && (t.vals.snakeLength == 0|| t.vals.add_y != -1) && !t.vals.pressed) {
+      // down
+      t.vals.add_x = 0;
+      t.vals.add_y = 1;
+      event.preventDefault();
+      event.returnValue = false;
+      t.vals.pressed = true;
+    }
   }
-}
-
-function drawGrid(interval) {
-  // draw a grid with the interval between lines in px for dev purposes
-  ctx.beginPath();
-  for (var x = interval; x < canvas.width; x=x+interval) {
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, canvas.height);
-  }
-
-  for (var y = interval; y < canvas.height; y=y+interval) {
-    ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
-  }
-  ctx.stroke(); 
-}
-
-function makeBorder() {
-  // fill in the border blocks as black
-  for (var i = 0; i < window.grid_width; i++) {
-    fillSquare(i, 0, true);
-    boundaries.push({x: i, y: 0});
-    fillSquare(i, window.grid_height - 1, true);
-    boundaries.push({x: i, y: window.grid_height - 1});
-  }
-  for (var i = 0; i < window.grid_height; i++) {
-    fillSquare(0, i, true);
-    boundaries.push({x: 0, y: i});
-    fillSquare(window.grid_width - 1, i, true);
-    boundaries.push({x: window.grid_width - 1, y: i});
-  }
-}
+};
