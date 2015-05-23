4d291cbe9f05f894c712656cd1e6e3405154b3a1
davis <dbh937@gmail.com>
2015-05-23 00:03:52 -0400

    clients can now post to the leaderboard and get the top 10 scores, as well as the user's own high score

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -5,12 +5,30 @@ module.exports = {
   get: function(req, res) {
     if (req.session.username) {
       res.type("text/json");
-      res.send({"username": req.session.username});
+      db.collection('scores').find({}).sort({score: -1}).limit(10).toArray(function(err, result) {
+        db.collection('scores').find({username: req.session.username}).sort({score:-1}).limit(1).toArray(function(err, r2) {
+          res.send({
+            "username": req.session.username,
+            "scores": result,
+            "highscore": r2 ? r2[0] : {score: 0}
+          })
+        });
+      });
     } else {
       res.redirect('/');
     }
   },
   post: function(req, res) {
-
+    db.collection("scores").insert({
+      fullname: req.session.fullname,
+      username: req.session.username,
+      timestamp: new Date(),
+      score: parseInt(req.body.score),
+      game: 'snake'
+    }, function(err, result) {
+      if (!err) {
+        res.send({success: 'true', result: result});
+      }
+    });
   }
 };
